cb3a726d32f718162101770c83915787b9b04f0a
Yassine <yachour92@gmail.com>
2024-07-10 19:06:09 +0200

    refacto(landing): sortir le wrapper style dans un fichier

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,29 +1,14 @@
 import React from "react";
-import styled from "styled-components";
-
-const StyledButton = styled.button`
-  background-color: red;
-  color: white;
-`;
+import { StyledButton, StyledWrapper } from "../assets/wrappers/LandingPage";
 
 const Landing = () => {
   return (
-    <Wrapper>
+    <StyledWrapper>
       <h1> Landing Page </h1>
       <StyledButton>styled btn</StyledButton>
-    </Wrapper>
+    </StyledWrapper>
   );
 };
 
-const Wrapper = styled.div`
-  background-color: red;
-  h1 {
-    color: white;
-  }
-  .content {
-    background: blue;
-    color: yellow;
-  }
-`;
 
 export default Landing;
