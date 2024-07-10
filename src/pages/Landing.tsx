4042ff2b98042aecda313920eb5b2327f1f0fd1e
Yassine <yachour92@gmail.com>
2024-07-10 18:50:34 +0200

    feat(landing): ajouter la page landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background-color: red;
+  color: white;
+`;
+
 const Landing = () => {
   return (
-    <h1> Landing Page
-      
-    </h1>
-  )
-}
+    <Wrapper>
+      <h1> Landing Page </h1>
+      <StyledButton>styled btn</StyledButton>
+    </Wrapper>
+  );
+};
 
-export default Landing
+const Wrapper = styled.div`
+  background-color: red;
+  h1 {
+    color: white;
+  }
+  .content {
+    background: blue;
+    color: yellow;
+  }
+`;
+
+export default Landing;
