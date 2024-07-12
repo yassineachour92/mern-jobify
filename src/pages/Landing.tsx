f02343ec044aba43d2aeca09b4121724632478c8
Yassine <yachour92@gmail.com>
2024-07-12 13:30:57 +0200

    refacto(css): travaille sur les fichiers Css des differents pages ainsi que la correction du probleme de l'svg

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,14 +1,49 @@
 import React from "react";
-import { StyledButton, StyledWrapper } from "../assets/wrappers/LandingPage";
+import logo from "../assets/logo.svg";
+import main from "../assets/main-alternative.svg";
+
+import { Link } from "react-router-dom";
+
+import '../pages/style/landing.css'
 
 const Landing = () => {
   return (
-    <StyledWrapper>
-      <h1> Landing Page </h1>
-      <StyledButton>styled btn</StyledButton>
-    </StyledWrapper>
+    <>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Le Lorem Ipsum est simplement du faux texte employé dans la
+            composition et la mise en page avant impression. Le Lorem Ipsum est
+            le faux texte standard de l'imprimerie depuis les années 1500, quand
+            un imprimeur anonyme assembla ensemble des morceaux de texte pour
+            réaliser un livre spécimen de polices de texte. Il n'a pas fait que
+            survivre cinq siècles, mais s'est aussi adapté à la bureautique
+            informatique, sans que son contenu n'en soit modifié. Il a été
+            popularisé dans les années 1960 grâce à la vente de feuilles
+            Letraset contenant des passages du Lorem Ipsum, et, plus récemment,
+            par son inclusion dans des applications de mise en page de texte,
+            comme Aldus PageMaker.
+          </p>
+          <div className="link-button">
+
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+          </div>
+          <img src={main} alt='job hunt' className="img main-img" />
+        </div>
+      </div>
+    </>
   );
 };
-
 
 export default Landing;
