5e680392223dbacc3998ec7f13850f86366a4876
Yassine <yachour92@gmail.com>
2024-07-13 00:28:51 +0200

    feat(dashboard): mettre la page de dashboard

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import SmallSidebar from "../components/SmallSidebar";
+import BigSidebar from "../components/BigSidebar";
+import Navbar from "../components/Navbar";
+
+import '../pages/style/dashboard.css'
 
 const DashBoard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const user={name:'john'}
 
-export default DashBoard
+  const[showSider,setShowSider ]=useState(false);
+  const[isDarkTheme,setisDarkTheme ]=useState(false)
+
+
+  return (
+    <main className="dashboard">
+      <SmallSidebar />
+      <BigSidebar />
+      <div>
+        <Navbar />
+        <div className="dashboard-page">
+          <Outlet />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default DashBoard;
