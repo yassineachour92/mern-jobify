f762b0b875a6a1e237b3ec362a8a85c7acad9772
Yassine <yachour92@gmail.com>
2024-07-15 15:40:20 +0200

    feat(bigSiderBar): trvailler sur le css de big sider bar

---
+++ src/components/BigSidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../pages/style/bigSidebar.css'
+
+const BigSidebar = () => {
+  return (
+    <div className='main'>
+      BigSidebar
+    </div>
+  )
+}
+
+export default BigSidebar
