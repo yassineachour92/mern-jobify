49c37e143aed88ede818336309549482ba21d486
Yassine <yachour92@gmail.com>
2024-07-17 15:36:11 +0200

    feat(bigSiderBar): re-travaille sur le big sider bar

--- src/components/BigSidebar.tsx
+++ src/components/BigSidebar.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import '../pages/style/bigSidebar.css'
+import React from "react";
+import { useDashBoardContext } from "../pages/DashBoard";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks.tsx";
 
+import "../pages/style/bigSidebar.css";
 const BigSidebar = () => {
+  const { showSiderBar } = useDashBoardContext();
   return (
-    <div className='main'>
-      BigSidebar
-    </div>
-  )
-}
+    <main className="big-sidebar-main">
+      <div
+        className={
+          showSiderBar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks isBigSidebar />
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default BigSidebar
+export default BigSidebar;
