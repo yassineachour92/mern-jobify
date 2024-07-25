d87bcc01fb5d4f11f7a06336a8121f44b7ef5191
Yassine <yachour92@gmail.com>
2024-07-25 13:28:23 +0200

    feat(pages): init les autres pages

--- src/pages/AllJobs.tsx
+++ src/pages/AllJobs.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AllJobs = () => {
   return (
     <div>
-      
+      all jobs
     </div>
   )
 }
--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Stats = () => {
   return (
     <div>
-      
+      <h1>stats</h1>
     </div>
   )
 }
